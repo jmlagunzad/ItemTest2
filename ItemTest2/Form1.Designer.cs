ad051028595e406fb5ee6ff34f30bb05bb64a7cc
may025jpl <may025jpl@gmail.com>
2019-05-28 11:43:10 +0800

    Add project files.

---
+++ ItemTest2/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace ItemTest2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.txtQty = new System.Windows.Forms.TextBox();
+            this.tblFruit = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.tblFruit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(82, 218);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(82, 260);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 1;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.BtnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(82, 301);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 2;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(82, 43);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 22);
+            this.txtId.TabIndex = 3;
+            this.txtId.Text = "id";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(82, 84);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 22);
+            this.txtName.TabIndex = 4;
+            this.txtName.Text = "name";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(82, 124);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 22);
+            this.txtPrice.TabIndex = 5;
+            this.txtPrice.Text = "price";
+            // 
+            // txtQty
+            // 
+            this.txtQty.Location = new System.Drawing.Point(82, 165);
+            this.txtQty.Name = "txtQty";
+            this.txtQty.Size = new System.Drawing.Size(100, 22);
+            this.txtQty.TabIndex = 6;
+            this.txtQty.Text = "qty";
+            // 
+            // tblFruit
+            // 
+            this.tblFruit.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tblFruit.Location = new System.Drawing.Point(273, 37);
+            this.tblFruit.Name = "tblFruit";
+            this.tblFruit.RowTemplate.Height = 24;
+            this.tblFruit.Size = new System.Drawing.Size(464, 246);
+            this.tblFruit.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tblFruit);
+            this.Controls.Add(this.txtQty);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.tblFruit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.TextBox txtQty;
+        private System.Windows.Forms.DataGridView tblFruit;
+    }
+}
+
---
+++ ItemTest2/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ItemTest2
+{
+    public partial class Form1 : Form
+    {
+
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-VQVN4SH;Initial Catalog=ItemTest;Integrated Security=True;Pooling=False");
+        SqlDataAdapter adapt;
+        SqlCommand comm;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                comm = new SqlCommand("INSERT INTO Fruit (id, fname, price, qty) VALUES (@id, @fname, @price, @qty)",con);
+                con.Open();
+                comm.Parameters.AddWithValue("@id", Int32.Parse(txtId.Text));
+                comm.Parameters.AddWithValue("@fname", txtName.Text);
+                comm.Parameters.AddWithValue("@price", Double.Parse(txtPrice.Text));
+                comm.Parameters.AddWithValue("@qty", Int32.Parse(txtQty.Text));
+                comm.ExecuteNonQuery();
+                con.Close();
+                UpdateTable();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Problem Adding");
+            }
+        }
+
+        private void BtnEdit_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                comm = new SqlCommand("UPDATE Fruit SET fname = @fname, price = @price, qty = @qty WHERE id = @id", con);
+                con.Open();
+                comm.Parameters.AddWithValue("@id", Int32.Parse(txtId.Text));
+                comm.Parameters.AddWithValue("@fname", txtName.Text);
+                comm.Parameters.AddWithValue("@price", Double.Parse(txtPrice.Text));
+                comm.Parameters.AddWithValue("@qty", Int32.Parse(txtQty.Text));
+                comm.ExecuteNonQuery();
+                con.Close();
+                UpdateTable();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Problem Editing");
+            }
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                comm = new SqlCommand("DELETE Fruit WHERE id = @id", con);
+                con.Open();
+                comm.Parameters.AddWithValue("@id", Int32.Parse(txtId.Text));
+                comm.ExecuteNonQuery();
+                con.Close();
+                UpdateTable();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Problem Deleting");
+            }
+        }
+
+
+        private void UpdateTable()
+        {
+            DisplayData();
+            ClearFields();
+        }
+
+        private void DisplayData()
+        {
+            con.Open();
+            DataTable dt = new DataTable();
+            adapt = new SqlDataAdapter("SELECT * FROM Fruit", con);
+            adapt.Fill(dt);
+            tblFruit.DataSource = dt;
+            con.Close();
+        }
+        //Clear Data  
+        private void ClearFields()
+        {
+            txtId.Text = "";
+            txtName.Text = "";
+            txtPrice.Text = "";
+            txtQty.Text = "";
+            
+        }
+
+    }
+}
